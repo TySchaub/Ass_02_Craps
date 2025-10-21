46a81db5b4eed99ab54c35c5fa54858efcc4cfc3
tyschaub <schaubtj@mail.uc.edu>
2025-10-20 21:45:52 -0400

    Variables

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
-public class Main {
-    public static void main(String[] args) {
+import java.util.Random;
+import java.util.Scanner;
 
-    }
-}
+public class Main
+{
+    public static void main(String[] args)
+    {
+        Scanner in = new Scanner(System.in);
+        Random rnd = new Random();
+        int die1;
+        int die2;
+        boolean done1 = false;
+        boolean done2 = false;
+        boolean validInput = false;
+        boolean roundTwoOver = false;
+        String inputYN;
+        int firstDiceSum;
+        int secondDiceSum;
